91b2addac89f5929e97a628eb0612bc32f3a25e7
midzer <midzer@gmail.com>
2019-02-20 17:14:58 +0100

    ignore case sensitivity for file types

--- js/tobi.js
+++ js/tobi.js
@@ -121,7 +121,7 @@
     var supportedElements = {
       image: {
         checkSupport: function (el) {
-          return !el.hasAttribute('data-type') && el.href.match(/\.(png|jpe?g|tiff|tif|gif|bmp|webp|svg|ico)$/)
+          return !el.hasAttribute('data-type') && el.href.match(/\.(png|jpe?g|tiff|tif|gif|bmp|webp|svg|ico)$/i)
         },
 
         init: function (el, container) {
